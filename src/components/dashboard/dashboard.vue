213178ab47637ce5a8628cf5ba98d900291e5f32
echikhhaythem1 <echikhhaythem20@gmail.com>
2020-08-19 16:15:12 +0100

    axios

---
+++ src/components/dashboard/dashboard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <vue-countdown v-on:time-expire="handleTimeExpire" :seconds="3600" :start="start"></vue-countdown>
+
+    <button v-on:click="startTimer">Start timer</button>
+  </div>
+</template>
+
+<script>
+import VueCountdown from '@dmaksimovic/vue-countdown';
+
+export default {
+  name: 'my-component',
+  data () {
+    return {
+      start: false
+    };
+  },
+  components: {
+    'vue-countdown': VueCountdown
+  },
+  methods: {
+    handleTimeExpire () {
+      alert('Time is up!')
+    },
+    startTimer () {
+      this.start = true
+    }
+  }
+}
+
+</script>
